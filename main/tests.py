465b69e1f4c02c0f80dc829660293fdb7cb3ac33
Alexander Charykov <monester@gmail.com>
2018-09-05 12:23:42 +0200

    fix issue (#1)
    * fix issue
    
    * break again

--- main/tests.py
+++ main/tests.py
@@ -3,7 +3,10 @@ from django.test import TestCase
 # Create your tests here.
 class TestMain(TestCase):
     def test_fail_math(self):
-        assert 1 + 1 == 3
+        assert 1 + 2 == 3
 
     def test_math(self):
         assert 1 + 1 == 2
+
+    def test_false(self):
+        assert False
